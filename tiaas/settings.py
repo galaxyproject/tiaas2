b2810c6f1fc5e4b27b80cf759243d7eab2944439
Helena Rasche <hxr@hx42.org>
2020-02-13 10:21:36 +0100

    move into a local settings file. ugh.

--- tiaas/settings.py
+++ tiaas/settings.py
@@ -77,20 +77,20 @@ WSGI_APPLICATION = "tiaas.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    },
-    "galaxy": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432",
-    },
-}
+# DATABASES = {
+    # "default": {
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    # },
+    # "galaxy": {
+        # "ENGINE": "django.db.backends.postgresql_psycopg2",
+        # "NAME": "postgres",
+        # "USER": "postgres",
+        # "PASSWORD": "postgres",
+        # "HOST": "localhost",
+        # "PORT": "5432",
+    # },
+# }
 
 
 # Password validation
