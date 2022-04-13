92a39d3d16eeeac814f34bf159092b2cc042d193
Cameron Hyde <chyde@neoformit.com>
2022-04-13 13:52:20 +1000

    Inject selected settings only to global templates

--- training/context_processors.py
+++ training/context_processors.py
@@ -6,5 +6,11 @@ from django.conf import settings as app_settings
 def settings(request):
     """Inject TIaaS settings."""
     return {
-        'settings': app_settings,
+        'settings': {
+            'TIAAS_OWNER': app_settings.TIAAS_OWNER,
+            'TIAAS_OWNER_SITE': app_settings.TIAAS_OWNER_SITE,
+            'TIAAS_GALAXY_SITE': app_settings.TIAAS_GALAXY_SITE,
+            'TIAAS_SHOW_ADVERTISING': app_settings.TIAAS_SHOW_ADVERTISING,
+            'TIAAS_GDPR_RETAIN_EXTRA': app_settings.TIAAS_GDPR_RETAIN_EXTRA,
+        },
     }
