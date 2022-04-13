06a6bed26afaa3ab9f620c309007522198e368d5
Cameron Hyde <chyde@neoformit.com>
2022-04-13 16:05:36 +1000

    Limit context processor to non-sensitive settings only

--- training/context_processors.py
+++ training/context_processors.py
@@ -8,7 +8,6 @@ def settings(request):
     return {
         'settings': {
             'TIAAS_OWNER': app_settings.TIAAS_OWNER,
-            'TIAAS_OWNER_SITE': app_settings.TIAAS_OWNER_SITE,
             'TIAAS_GALAXY_SITE': app_settings.TIAAS_GALAXY_SITE,
             'TIAAS_SHOW_ADVERTISING': app_settings.TIAAS_SHOW_ADVERTISING,
             'TIAAS_GDPR_RETAIN_EXTRA': app_settings.TIAAS_GDPR_RETAIN_EXTRA,
