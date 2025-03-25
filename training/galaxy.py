611816d4c3738777b216ad18844c760ac0e23ce5
Mira Kuntz <mira.kuntz@yahoo.com>
2025-03-25 09:26:15 +0100

    on conflict is much simpler, thanks to bgruening

--- training/galaxy.py
+++ training/galaxy.py
@@ -203,17 +203,11 @@ def create_group(training_id, role_id):
 
 
 def add_group_user(group_id, user_id):
-    existing = fetch_one(
-        "select 1 from user_group_association where user_id = %s and group_id = %s",
+    execute(
+        "insert into user_group_association (user_id, group_id, create_time, update_time) "
+        "values (%s, %s, now(), now()) on conflict do nothing",
         (user_id, group_id),
     )
-
-    if existing is None:
-        execute(
-            "insert into user_group_association (user_id, group_id, create_time, update_time) "
-            "values (%s, %s, now(), now())",
-            (user_id, group_id),
-        )
 
 
 def execute(query, params=None):
@@ -233,13 +227,6 @@ def execute_txn(query, params=None, commit=False):
                 return result
         except IntentionalRollback:
             pass
-
-
-def fetch_one(query, params=None):
-    with connections["galaxy"].cursor() as cursor:
-        cursor.execute(query, params)
-        result = cursor.fetchone()  # fetch one row
-    return result
 
 
 def fetch_all(query, params=None):
